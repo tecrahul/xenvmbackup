79662fe72645e13ec40bdab98f1a9a7481dea443
kerazo <info@kerazo.de>
2020-01-20 15:35:41 +0100

    && error 0 now correctly and mount -t NFS

--- xenvmbackup.sh
+++ xenvmbackup.sh
@@ -20,18 +20,18 @@ mkdir -p ${MOUNTPOINT}
 
 ### Mounting remote nfs share backup drive
 
-[ ! -d ${MOUNTPOINT} ]  && echo "No mount point found, kindly check"; exit 0
-mount -F nfs ${NFS_SERVER_IP}:${FILE_LOCATION_ON_NFS} ${MOUNTPOINT}
+[ ! -d ${MOUNTPOINT} ]  && echo "No mount point found, kindly check" && exit 0
+mount -t nfs ${NFS_SERVER_IP}:${FILE_LOCATION_ON_NFS} ${MOUNTPOINT}
 
 BACKUPPATH=${MOUNTPOINT}/${XSNAME}/${DATE}
 mkdir -p ${BACKUPPATH}
-[ ! -d ${BACKUPPATH} ]  && echo "No backup directory found"; exit 0
+[ ! -d ${BACKUPPATH} ]  && echo "No backup directory found" && exit 0
 
 
 # Fetching list UUIDs of all VMs running on XenServer
 xe vm-list is-control-domain=false is-a-snapshot=false | grep uuid | cut -d":" -f2 > ${UUIDFILE}
 
-[ ! -f ${UUIDFILE} ] && echo "No UUID list file found"; exit 0
+[ ! -f ${UUIDFILE} ] && echo "No UUID list file found" && exit 0
 
 while read VMUUID
 do
